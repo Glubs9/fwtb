3a57213c07881174222829d1f5d58baf04c821ac
glubs9 <jonte.fry@gmail.com>
2021-06-13 16:25:33 +1000

    switched to i-- because i'm an idiot

--- execute_word/execute_user_code.c
+++ execute_word/execute_user_code.c
@@ -3,11 +3,23 @@
 #include "../dictionary/user_code.h"
 #include "../stack/stack.h"
 
+void print_debug(user_code *uc)
+{
+	printf("printing debug\n");
+	printf("uc pointer %d\n", uc);
+	printf("uc size %d\n",uc->size);
+	for (int i = 0; i < uc->size; i++) {
+		printf("%s \n", uc->words[i]->name);
+	}
+	printf("end debug\n");
+}
+
 void execute_user_code(dict_node *dn, stack *call_stack) 
 {
 	user_code *uc;
-	uc = extract_user_code(dn);
-	for (int i = uc->size-1; i >= 0; i++) {
+	uc = extract_user_code(dn); //is this is a pointer?
+	//print_debug(uc);
+	for (int i = uc->size-1; i >= 0; i--) {
 		printf("pushing to stack \n");
 		push_stack(call_stack, uc->words[i]);
 		printf("pushed to call stack\n");
